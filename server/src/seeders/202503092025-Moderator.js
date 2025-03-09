5d8b43fea3e355da030b8ad2755bd19dd9d47251
Olha Shtepa <olgaolga2414@gmail.com>
2025-03-09 19:54:01 +0200

    seed moderator added&edited readme.md

---
+++ server/src/seeders/202503092025-Moderator.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      id: 3,  
+      firstName: 'mark',
+      lastName: 'mark',
+      displayName: 'mark',
+      password: '$2b$05$rkkLdnqVnLLHMlNtUJCc9uLnaENzzyHAilj7BP6CxAXmIp9PPjUlC', 
+      email: 'mark@gmail.com',
+      avatar: 'anon.png',
+      role: 'moderator',
+      balance: 0,
+      accessToken: null,
+      rating: 0
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', { email: 'moderator@example.com' }, {});
+  }
+};
