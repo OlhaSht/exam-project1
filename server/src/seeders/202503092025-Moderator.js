631542fb3a9c40669d020c33d7cede6b138c3fa1
Olha Shtepa <olgaolga2414@gmail.com>
2025-03-18 22:49:59 +0200

    update seed mjderator whith bcrypt&upsert

--- server/src/seeders/202503092025-Moderator.js
+++ server/src/seeders/202503092025-Moderator.js
@@ -1,23 +1,52 @@
 'use strict';
 
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     return queryInterface.bulkInsert('Users', [{
+//       id: 3,  
+//       firstName: 'mark',
+//       lastName: 'mark',
+//       displayName: 'mark',
+//       password: '$2b$05$rkkLdnqVnLLHMlNtUJCc9uLnaENzzyHAilj7BP6CxAXmIp9PPjUlC', 
+//       email: 'mark@gmail.com',
+//       avatar: 'anon.png',
+//       role: 'moderator',
+//       balance: 0,
+//       accessToken: null,
+//       rating: 0
+//     }], {});
+//   },
+
+//   down: async (queryInterface, Sequelize) => {
+//     return queryInterface.bulkDelete('Users', { email: 'moderator@example.com' }, {});
+//   }
+// };
+
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const CONSTANTS = require('../constants');
+const { Users} = require('../models');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [{
-      id: 3,  
-      firstName: 'mark',
-      lastName: 'mark',
-      displayName: 'mark',
-      password: '$2b$05$rkkLdnqVnLLHMlNtUJCc9uLnaENzzyHAilj7BP6CxAXmIp9PPjUlC', 
-      email: 'mark@gmail.com',
-      avatar: 'anon.png',
-      role: 'moderator',
-      balance: 0,
-      accessToken: null,
-      rating: 0
-    }], {});
+    const hashedPassword = bcrypt.hashSync(process.env.MODERATOR_PASSWORD, CONSTANTS.SALT_ROUNDS); 
+    
+    await Users.upsert( 
+      {
+        firstName: 'mark',
+        lastName: 'mark',
+        displayName: 'mark',
+        password: hashedPassword, 
+        email: 'mark@gmail.com',
+        avatar: 'anon.png',
+        role: 'moderator',
+        balance: 0,
+        rating: 0,
+      },
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Users', { email: 'moderator@example.com' }, {});
-  }
+    await queryInterface.bulkDelete('Users', { email: 'mark@gmail.com' });
+  },
 };
