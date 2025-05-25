df74dfeabf0f4b0a25bbe5620ab552e92c89c16f
Olha Shtepa <olgaolga2414@gmail.com>
2025-05-25 17:12:02 +0300

    postgresConfig edited

---
+++ server/src/config/postgresConfig.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST_DEV,
+    dialect: 'postgres',
+    operatorsAliases: 'Op',
+    seederStorage: 'sequelize',
+  },
+  test: {
+    username: process.env.DB_USER_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
+    dialect: 'postgres',
+    operatorsAliases: 'Op',
+    seederStorage: 'sequelize',
+  },
+  production: {
+    username: process.env.DB_USER_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST_PROD,
+    dialect: 'postgres',
+    operatorsAliases: 'Op',
+    seederStorage: 'sequelize',
+  }
+};
