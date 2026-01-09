3235941b13eeaeabc15fbc1cda4033bebeb8727b
Olha Shtepa <olgaolga2414@gmail.com>
2026-01-09 19:04:38 +0200

    Improve image offer preview in moderator page

--- client/src/pages/ModeratorPage/ModeratorPage.jsx
+++ client/src/pages/ModeratorPage/ModeratorPage.jsx
@@ -1,19 +1,21 @@
-
 import React, { useEffect } from 'react';
-import styles from './ModeratorPage.module.sass'
+import styles from './ModeratorPage.module.sass';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   getModeratorOffers,
   approveModeratorOffer,
   rejectModeratorOffer,
 } from '../../store/slices/moderatorSlice';
+import CONSTANTS from '../../constants';
 
 const ModeratorPage = () => {
   const dispatch = useDispatch();
-  const { offers, isFetching, error, currentPage, totalPages } = useSelector((state) => state.moderator);
+  const { offers, isFetching, error, currentPage, totalPages } = useSelector(
+    (state) => state.moderator
+  );
 
   useEffect(() => {
-    dispatch(getModeratorOffers({page: currentPage}));
+    dispatch(getModeratorOffers({ page: currentPage }));
   }, [dispatch, currentPage]);
 
   const handlePageChange = (newPage) => {
@@ -53,38 +55,55 @@ const ModeratorPage = () => {
 
   return (
     <div className={styles.moderatorContainer}>
-      <section className = {styles.tableContainer}>
-       
-          <div className = {styles.inputWrapper}>
-            <h1>Offers</h1>
-          </div>
-        
+      <section className={styles.tableContainer}>
+        <div className={styles.inputWrapper}>
+          <h1>Offers</h1>
+        </div>
+
         <article>
           {offers.length > 0 ? (
             offers.map((offer) => (
-            <div className = {styles.inputWrapper} key={offer.id}>
-            <div className = {styles.wrapperInputInfo}>
-              <div className={styles.inputInfo}>{offer.User?.firstName} {offer.User?.lastName}</div>
-              <div className={styles.inputInfo}>{offer.text || 'No details available'}</div>
-              <div className={styles.inputInfo}>{offer.moderatorStatus || 'Pending'}</div>
-              <div className={styles.inputInfo}>
-                <button
-                  className={styles.buttonApprove}
-                  onClick={() => handleApprove(offer.id)}
-                  disabled={offer.moderatorStatus === 'approved'}
-                >
-                  Approve
-                </button>
-                <button
-                  className={styles.buttonReject}
-                  onClick={() => handleReject(offer.id)}
-                  disabled={offer.moderatorStatus === 'rejected'}
-                >
-                  Reject
-                </button>
+              <div className={styles.inputWrapper} key={offer.id}>
+                <div className={styles.wrapperInputInfo}>
+                  <div className={styles.inputInfo}>
+                    {offer.User?.firstName} {offer.User?.lastName}
+                  </div>
+                  {/* <div className={styles.inputInfo}>{offer.text || 'No details available'}</div> */}
+                  <div className={styles.inputInfo}>
+                    {offer.text && offer.text}
+
+                    {!offer.text && offer.fileName && (
+                      <img
+                        src={`${CONSTANTS.publicURL}${offer.fileName}`}
+                        alt="offer preview"
+                        className={styles.offerImage}
+                      />
+                    )}
+
+                    {!offer.text && !offer.fileName && 'No details available'}
+                  </div>
+
+                  <div className={styles.inputInfo}>
+                    {offer.moderatorStatus || 'Pending'}
+                  </div>
+                  <div className={styles.inputInfo}>
+                    <button
+                      className={styles.buttonApprove}
+                      onClick={() => handleApprove(offer.id)}
+                      disabled={offer.moderatorStatus === 'approved'}
+                    >
+                      Approve
+                    </button>
+                    <button
+                      className={styles.buttonReject}
+                      onClick={() => handleReject(offer.id)}
+                      disabled={offer.moderatorStatus === 'rejected'}
+                    >
+                      Reject
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-            </div>
             ))
           ) : (
             <p>No offers available</p>
@@ -98,7 +117,9 @@ const ModeratorPage = () => {
           >
             Back
           </button>
-          <span className={styles.pageNamber}>Page {currentPage} of {totalPages}</span>
+          <span className={styles.pageNamber}>
+            Page {currentPage} of {totalPages}
+          </span>
           <button
             className={styles.buttonNext}
             disabled={currentPage === totalPages}
@@ -113,4 +134,3 @@ const ModeratorPage = () => {
 };
 
 export default ModeratorPage;
-
