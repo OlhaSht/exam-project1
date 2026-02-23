639625bd75a0d4361fd2b5a1cc1cd660510b04c0
Olha Shtepa <olgaolga2414@gmail.com>
2026-02-23 18:50:25 +0200

    remove <React.StrictMode> from index

--- client/src/index.js
+++ client/src/index.js
@@ -16,12 +16,12 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-  );
+  // <React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  // </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
