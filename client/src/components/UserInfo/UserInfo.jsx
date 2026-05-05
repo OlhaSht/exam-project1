6fd69a1a727b56c51c4244eee3ab7c63a3b62a3b
Olha Shtepa <olgaolga2414@gmail.com>
2026-05-05 20:53:13 +0300

    adding object-fit: contain for user avatar

--- client/src/components/UserInfo/UserInfo.jsx
+++ client/src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import UpdateUserInfoForm from '../UpdateUserInfoForm/UpdateUserInfoForm';
 import { updateUser } from '../../store/slices/userSlice';
