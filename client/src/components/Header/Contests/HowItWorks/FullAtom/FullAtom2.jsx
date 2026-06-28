666e4f452ad823d72288200f7c84fb51289beabb
Olha Shtepa <olgaolga2414@gmail.com>
2026-06-28 21:16:33 +0300

    created structure FullAtom2

---
+++ client/src/components/Header/Contests/HowItWorks/FullAtom/FullAtom2.jsx
@@ -0,0 +1,37 @@
+import './Reset.css';
+import './Font.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './FullAtom2.css';
+import TopBanner from '../FullAtom/TopBanner/TopBanner.jsx';
+import Navigation from '../FullAtom/Navigation/Navigation.jsx';
+import HeroSection from '../FullAtom/HeroSection/HeroSection.jsx';
+import WaysSection from '../FullAtom/WaysSection/WaysSection.jsx';
+import WinSection from '../FullAtom/WinSection/WinSection.jsx';
+import QuestionsSection from '../FullAtom/QuestionsSection/QuestionsSection.jsx';
+import SearchSection from '../FullAtom/SearchSection/SearchSection.jsx';
+import FooterListSection from '../FullAtom/FooterListSection/FooterListSection.jsx';
+import AtomFooter from '../FullAtom/AtomFooter/AtomFooter.jsx';
+
+const FullAtom = () => {
+  const customFontStyle = {
+    fontFamily: 'Product Sans, sans-serif',
+  };
+
+  return (
+    <div className="body" style={customFontStyle}>
+      <header className="header">
+        <TopBanner />
+        <Navigation />
+        <HeroSection />
+        <WaysSection />
+        <WinSection />
+        <QuestionsSection />
+        <SearchSection />
+        <FooterListSection />
+        <AtomFooter />
+      </header>
+    </div>
+  );
+};
+
+export default FullAtom;
