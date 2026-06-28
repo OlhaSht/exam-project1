3e6610922d5b7816e29e1f32b6e0040bd7e38786
Olha Shtepa <olgaolga2414@gmail.com>
2026-06-28 19:35:57 +0300

    refactor: separated the structure Logo

---
+++ client/src/components/Header/Contests/HowItWorks/FullAtom/Navigation/Logo/Logo.jsx
@@ -0,0 +1,15 @@
+import styles from './Logo.css';
+
+const Logo = () => {
+  return (
+    <a href="/">
+      <img
+        className="main-logo"
+        src="https://img.atom.com/public/images/atom-logo.png"
+        alt="logo 'atom'"
+      />
+    </a>
+  );
+};
+
+export default Logo;
